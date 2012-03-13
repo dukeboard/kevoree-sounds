8ac225f02eacab2540e6360eea34788079adf4bf
Erwan Daubert <Erwan.Daubert@gmail.com>
2012-03-13 22:01:44 +0100

    remove .DS_Store fix .gitignore add API

---
+++ org.kevoree.ksounds.api/src/main/java/org/kevoree/ksounds/api/AudioRenderer.java
@@ -0,0 +1,30 @@
+package org.kevoree.ksounds.api;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 13/03/12
+ * Time: 20:55
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+
+import org.kevoree.annotation.*;
+import org.kevoree.framework.AbstractComponentType;
+
+@ComponentFragment
+@MessageTypes({
+        @MessageType(name = "format", elems = {@MsgElem(name = "bitrate", className = Float.class), @MsgElem(name = "channels", className = Integer.class), @MsgElem(name = "samplerate", className = Float.class)}),
+		@MessageType(name = "format", elems = {@MsgElem(name = "data", className = byte[].class)})
+})
+@Provides({
+		@ProvidedPort(name = "data", type = PortType.MESSAGE, messageType = "data"),
+		@ProvidedPort(name = "format", type = PortType.MESSAGE, messageType = "format")
+})
+public abstract class AudioRenderer extends AbstractComponentType {
+	
+	@Port(name = "format")
+	public abstract void configureFormat(Object message);
+	
+	public abstract void writeData(Object message);
+}
